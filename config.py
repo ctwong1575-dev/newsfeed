6ad2aa4b414092c56335d0f1a6c3907abda8f6d2
ctwong1575-dev <ctwong1575@gmail.com>
2026-08-26 01:49:53 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,10 +7,12 @@ GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "615899930")
 
+# 已替換為最新且每日高頻更新的財經 RSS 來源
 NEWS_FEEDS = {
-    "CNBC": "https://search.cnbc.com/rs/search/combinedrender.view?partnerId=wrss01&id=10000664&profile=rss&wants=news",
-    "CNN Business": "http://rss.cnn.com/rss/money_latest.rss",
-    "DW Business": "https://rss.dw.com/xml/rss-en-bus",
-    "Sky News Business": "https://feeds.skynews.com/feeds/rss/business.rss",
-    "Al Arabiya English": "https://english.alarabiya.net/tools/rss"
+    "CNBC Top News": "https://search.cnbc.com/rs/search/combinedrender.view?partnerId=wrss01&id=100003114&profile=rss&wants=news",
+    "CNBC Finance": "https://search.cnbc.com/rs/search/combinedrender.view?partnerId=wrss01&id=10000664&profile=rss&wants=news",
+    "BBC Business": "http://feeds.bbci.co.uk/news/business/rss.xml",
+    "MarketWatch Top Stories": "http://feeds.marketwatch.com/marketwatch/topstories/",
+    "Yahoo Finance": "https://finance.yahoo.com/news/rssindex",
+    "Sky News Business": "https://feeds.skynews.com/feeds/rss/business.rss"
 }
